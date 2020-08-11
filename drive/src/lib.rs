3cfc05b9c04c655fe8d58f2d3bbc78902adead05
Jess Frazelle <jess@oxide.computer>
2020-08-11 14:09:32 -0700

    update drive
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- drive/src/lib.rs
+++ drive/src/lib.rs
@@ -207,9 +207,15 @@ impl GoogleDrive {
         match resp.status() {
             StatusCode::OK => (),
             s => {
+                let text = resp.text().await.unwrap();
+
+                if text.contains("Use Export with Google Docs files") {
+                    return self.get_file_contents_by_id(id).await;
+                }
+
                 return Err(APIError {
                     status_code: s,
-                    body: resp.text().await.unwrap(),
+                    body: text,
                 });
             }
         };
@@ -221,7 +227,7 @@ impl GoogleDrive {
     pub async fn get_file_contents_by_id(
         &self,
         id: &str,
-    ) -> Result<String, APIError> {
+    ) -> Result<Bytes, APIError> {
         let file = self.get_file_by_id(id).await.unwrap();
 
         // Build the request.
@@ -247,7 +253,7 @@ impl GoogleDrive {
         };
 
         // Try to deserialize the response.
-        Ok(resp.text().await.unwrap())
+        Ok(resp.bytes().await.unwrap())
     }
 
     /// Get a file by it's ID.
