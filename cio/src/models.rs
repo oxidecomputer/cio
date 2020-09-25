c6ab81e3a8f2a901b05e7e6b940ba016397cc4cd
Jess Frazelle <jess@oxide.computer>
2020-09-25 13:08:40 -0700

    add contractor as a status
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- cio/src/models.rs
+++ cio/src/models.rs
@@ -168,6 +168,8 @@ impl NewApplicant {
                 s = "Declined".to_string();
             } else if s.contains("hired") {
                 s = "Hired".to_string();
+            } else if s.contains("contractor") {
+                s = "Contractor".to_string();
             } else {
                 s = "Needs to be triaged".to_string();
             }
