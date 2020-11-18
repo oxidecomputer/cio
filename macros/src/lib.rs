4f18265b529767e417b06d84905a9cc53ac4999a
Jess Frazelle <jess@oxide.computer>
2020-11-18 11:39:14 -0800

    working
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -31,6 +31,7 @@ fn do_db_struct(
 ) -> Result<TokenStream, Error> {
     let metadata = from_tokenstream::<Metadata>(&attr).unwrap();
     let new_name = format_ident!("{}", metadata.new_name);
+    let new_name_plural = format_ident!("{}s", metadata.new_name);
 
     let old_struct: ItemStruct = syn::parse2(item.clone()).unwrap();
     let mut fields: Vec<&Field> = Default::default();
@@ -68,7 +69,69 @@ fn do_db_struct(
                 println!("created new row in airtable: {:?}", self);
             }
         }
-            );
+
+        pub struct #new_name_plural(pub Vec<#new_name>);
+        impl #new_name_plural {
+            /// Update Airtable records in a table from a vector.
+            async fn update_airtable(self) {
+                // Initialize the Airtable client.
+                let airtable = airtable_api::Airtable::new(
+                    airtable_api::api_key_from_env(),
+                    #base_id,
+                );
+
+                let result: Vec<airtable_api::Record<#new_name>> = airtable
+                    .list_records(#table, "Grid view", vec![])
+                    .await
+                    .unwrap();
+
+                let mut records: std::collections::BTreeMap<i32, airtable_api::Record<#new_name>> =
+                    Default::default();
+                for record in result {
+                    records.insert(record.fields.id, record);
+                }
+
+                for vec_record in self.0 {
+                    // See if we have it in our Airtable records.
+                    match records.get(&vec_record.id) {
+                        Some(r) => {
+                            // If the Airtable record and the vector record are the same, then we can return early since
+                            // we do not need to update it in Airtable.
+                            if vec_record == r.fields {
+                                println!("[airtable] id={} in vector equals Airtable record, skipping update", vec_record.id);
+                                continue;
+                            }
+
+                            // Let's update the Airtable record with the record from the vector.
+                            let mut record = r.clone();
+                            record.fields = vec_record.clone();
+
+                            airtable
+                                .update_records(
+                                    #table,
+                                    vec![record.clone()],
+                                )
+                                .await
+                                .unwrap();
+                            println!(
+                                "[airtable] id={} updated in Airtable",
+                                vec_record.id
+                            );
+                        }
+                        None => {
+                            // We do not have the record in Airtable, let's create it.
+                            // Create the record in Airtable.
+                            vec_record.push_to_airtable().await;
+                            println!(
+                                "[airtable] id={} created in Airtable",
+                                vec_record.id
+                            );
+                        }
+                    }
+                }
+            }
+        }
+                    );
     }
 
     let new_struct = quote!(
@@ -117,52 +180,114 @@ mod tests {
             .into(),
         );
         let expected = quote! {
-            pub struct Item {
-                pub foo: String,
-                pub bar: String
+        pub struct Item {
+            pub foo: String,
+            pub bar: String
+        }
+
+        #[derive(
+            Debug,
+            Queryable,
+            Identifiable,
+            Associations,
+            PartialEq,
+            Clone,
+            JsonSchema,
+            Deserialize,
+            Serialize,
+        )]
+        pub struct DuplicatedItem {
+            pub id: i32,
+            pub foo: String,
+            pub bar: String
+        }
+
+        impl DuplicatedItem {
+            /// Push the row to our Airtable workspace.
+            pub async fn push_to_airtable(&self) {
+                // Initialize the Airtable client.
+                let airtable =
+                    airtable_api::Airtable::new(airtable_api::api_key_from_env(), AIRTABLE_BASE_ID_CUSTOMER_LEADS);
+
+                // Create the record.
+                let record = airtable_api::Record {
+                    id: None,
+                    created_time: None,
+                    fields: self.clone(),
+                };
+
+                // Send the new record to the Airtable client.
+                // Batch can only handle 10 at a time.
+                let _ : Vec<airtable_api::Record<DuplicatedItem>> = airtable
+                    .create_records(AIRTABLE_RFD_TABLE, vec![record])
+                    .await
+                    .unwrap();
+
+                println!("created new row in airtable: {:?}", self);
             }
+        }
 
-            #[derive(
-                Debug,
-                Queryable,
-                Identifiable,
-                Associations,
-                PartialEq,
-                Clone,
-                JsonSchema,
-                Deserialize,
-                Serialize,
-            )]
-            pub struct DuplicatedItem {
-                pub id: i32,
-                pub foo: String,
-                pub bar: String
-            }
+        pub struct DuplicatedItems(pub Vec<DuplicatedItem>);
+        impl DuplicatedItems {
+            /// Update Airtable records in a table from a vector.
+            async fn update_airtable(self) {
+                // Initialize the Airtable client.
+                let airtable = airtable_api::Airtable::new(
+                    airtable_api::api_key_from_env(),
+                    AIRTABLE_BASE_ID_CUSTOMER_LEADS,
+                );
 
-            impl DuplicatedItem {
-                /// Push the row to our Airtable workspace.
-                pub async fn push_to_airtable(&self) {
-                    // Initialize the Airtable client.
-                    let airtable =
-                        airtable_api::Airtable::new(airtable_api::api_key_from_env(), AIRTABLE_BASE_ID_CUSTOMER_LEADS);
+                let result: Vec<airtable_api::Record<DuplicatedItem>> = airtable
+                    .list_records(AIRTABLE_RFD_TABLE, "Grid view", vec![])
+                    .await
+                    .unwrap();
 
-                    // Create the record.
-                    let record = airtable_api::Record {
-                        id: None,
-                        created_time: None,
-                        fields: self.clone(),
-                    };
+                let mut records: std::collections::BTreeMap<i32, airtable_api::Record<DuplicatedItem>> =
+                    Default::default();
+                for record in result {
+                    records.insert(record.fields.id, record);
+                }
 
-                    // Send the new record to the Airtable client.
-                    // Batch can only handle 10 at a time.
-                    let _ : Vec<airtable_api::Record<DuplicatedItem>> = airtable
-                        .create_records(AIRTABLE_RFD_TABLE, vec![record])
-                        .await
-                        .unwrap();
+                for vec_record in self.0 {
+                    // See if we have it in our Airtable records.
+                    match records.get(&vec_record.id) {
+                        Some(r) => {
+                            // If the Airtable record and the vector record are the same, then we can return early since
+                            // we do not need to update it in Airtable.
+                            if vec_record == r.fields {
+                                println!("[airtable] id={} in vector equals Airtable record, skipping update", vec_record.id);
+                                continue;
+                            }
 
-                    println!("created new row in airtable: {:?}", self);
+                            // Let's update the Airtable record with the record from the vector.
+                            let mut record = r.clone();
+                            record.fields = vec_record.clone();
+
+                            airtable
+                                .update_records(
+                                    AIRTABLE_RFD_TABLE,
+                                    vec![record.clone()],
+                                )
+                                .await
+                                .unwrap();
+                            println!(
+                                "[airtable] id={} updated in Airtable",
+                                vec_record.id
+                            );
+                        }
+                        None => {
+                            // We do not have the record in Airtable, let's create it.
+                            // Create the record in Airtable.
+                            vec_record.push_to_airtable().await;
+                            println!(
+                                "[airtable] id={} created in Airtable",
+                                vec_record.id
+                            );
+                        }
+                    }
                 }
             }
+        }
         };
 
         assert_eq!(expected.to_string(), ret.unwrap().to_string());
