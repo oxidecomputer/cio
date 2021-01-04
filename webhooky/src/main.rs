88ecea16ba0810248c454ff2dc6013e206439b39
Jess Frazelle <jess@oxide.computer>
2021-01-04 10:08:59 -0800

    update
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- webhooky/src/main.rs
+++ webhooky/src/main.rs
@@ -767,9 +767,14 @@ async fn listen_airtable_shipments_outgoing_edit_webhooks(_rqctx: Arc<RequestCon
 #[inline]
 async fn listen_shippo_tracking_update_webhooks(_rqctx: Arc<RequestContext>, body_param: TypedBody<serde_json::Value>) -> Result<HttpResponseAccepted<String>, HttpError> {
     let event = body_param.into_inner();
-    event!(Level::DEBUG, "{:?}", event);
-    println!("shippo-tracking-update: {:?}", event);
-    let body: shippo::TrackingStatus = serde_json::from_str(&event.to_string()).unwrap();
+    let body: shippo::TrackingStatus = serde_json::from_str(&event.to_string()).unwrap_or_default();
+
+    if body.address_from.street1.is_empty() {
+        // We can reaturn early.
+        // It's too early to get anything good from this event.
+        Ok(HttpResponseAccepted("ok".to_string()))
+    }
+
     println!("shippo-tracking-update parsed: {:?}", body);
 
     //event!(Level::INFO, "shipment {} tracking status updated successfully", a.email);
