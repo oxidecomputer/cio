151316a36b4a6410ca18f2e8ee5bddf39cfaa4e8
Jess Frazelle <jess@oxide.computer>
2021-02-09 12:20:49 -0800

    do pdf unite in rust not binary
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- cio/src/interviews.rs
+++ cio/src/interviews.rs
@@ -1,14 +1,14 @@
 #![allow(clippy::from_over_into)]
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::env;
 use std::fs;
-use std::io::{copy, stderr, stdout, Read, Write};
-use std::process::Command;
+use std::io::{copy, Write};
 
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use google_drive::GoogleDrive;
 use gsuite_api::GSuite;
+use lopdf::{Bookmark, Document, Object, ObjectId};
 use macros::db;
 use pandoc::OutputKind;
 use schemars::JsonSchema;
@@ -157,6 +157,11 @@ pub async fn refresh_interviews(db: &Database) {
 
             interview.name = format!("{} ({})", name, interviewers.join(", "));
 
+            if interview.interviewers.is_empty() {
+                // Continue early.
+                // We only care about interviews where the candidate has interviewers.
+                continue;
+            }
             interview.upsert(&db).await;
         }
     }
@@ -196,7 +201,10 @@ pub async fn compile_packets(db: &Database) {
             let applicant = Applicant::get_from_db(&db, employee.recovery_email.to_string(), sheet_id.to_string());
             if let Some(a) = applicant {
                 materials_url = a.materials;
-                break;
+                if !materials_url.is_empty() {
+                    // Sometimes we get back an empty materials url somehow...?
+                    break;
+                }
             }
         }
 
@@ -220,7 +228,12 @@ pub async fn compile_packets(db: &Database) {
         }
         for interviewer in interview.interviewers {
             let username = interviewer.trim_end_matches(GSUITE_DOMAIN).trim_end_matches(DOMAIN).trim_end_matches('@').trim().to_string();
-            let user = User::get_from_db(db, username.to_string()).unwrap();
+            let u = User::get_from_db(db, username.to_string());
+            if u.is_none() {
+                // Likeley means the person quit or no longer works here.
+                continue;
+            }
+            let user = u.unwrap();
             existing.push((user, interview.start_time, interview.end_time));
             interviewers.insert(interview.email.to_string(), existing.clone());
         }
@@ -339,29 +352,13 @@ The Oxide Team
     }
 
     // Concatenate all the files.
-    for (applicant_email, mut packet_args) in packets {
+    for (applicant_email, packet_args) in packets {
         for (_, sheet_id) in get_sheets_map() {
             let a = Applicant::get_from_db(&db, applicant_email.to_string(), sheet_id.to_string());
             if let Some(mut applicant) = a {
-                let mut output = env::temp_dir();
                 let filename = format!("Interview Packet - {}.pdf", applicant.name);
-                output.push(filename.to_string());
-                packet_args.push(output.to_str().unwrap().to_string());
 
-                // Combine all the PDFs into one PDF
-                let cmd_output = Command::new("pdfunite").args(&packet_args).output().unwrap();
-                match fs::read_to_string(output.clone()) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        println!("[applicants] running pdfunite failed: {} ", e);
-                        stdout().write_all(&cmd_output.stdout).unwrap();
-                        stderr().write_all(&cmd_output.stderr).unwrap();
-                    }
-                };
-                // Read the contents of the output file.
-                let mut f = fs::File::open(&output).unwrap();
-                let mut buffer = Vec::new();
-                f.read_to_end(&mut buffer).unwrap();
+                let buffer = combine_pdfs(packet_args);
 
                 // Create or update the file in the google_drive.
                 drive_client.create_or_upload_file(&drive_id, &parent_id, &filename, "application/pdf", &buffer).await.unwrap();
@@ -469,6 +466,158 @@ pub async fn download_materials(drive_client: &GoogleDrive, url: &str, username:
     pandoc.execute().unwrap();
 }
 
+/// Combine multiple pdfs into one pdf and return the byte stream of it.
+#[instrument]
+#[inline]
+pub fn combine_pdfs(pdfs: Vec<String>) -> Vec<u8> {
+    // Define a starting max_id (will be used as start index for object_ids)
+    let mut max_id = 1;
+    let mut pagenum = 1;
+    // Collect all Documents Objects grouped by a map
+    let mut documents_pages = BTreeMap::new();
+    let mut documents_objects = BTreeMap::new();
+    let mut document = Document::with_version("1.5");
+
+    for pdf in pdfs {
+        // Load the pdf as a file.
+        let mut doc = Document::load(&pdf).unwrap();
+
+        let mut first = false;
+        doc.renumber_objects_with(max_id);
+
+        max_id = doc.max_id + 1;
+
+        documents_pages.extend(
+            doc.get_pages()
+                .into_iter()
+                .map(|(_, object_id)| {
+                    if !first {
+                        let bookmark = Bookmark::new(format!("Page_{}", pagenum), [0.0, 0.0, 1.0], 0, object_id);
+                        document.add_bookmark(bookmark, None);
+                        first = true;
+                        pagenum += 1;
+                    }
+
+                    (object_id, doc.get_object(object_id).unwrap().to_owned())
+                })
+                .collect::<BTreeMap<ObjectId, Object>>(),
+        );
+        documents_objects.extend(doc.objects);
+    }
+
+    // Catalog and Pages are mandatory
+    let mut catalog_object: Option<(ObjectId, Object)> = None;
+    let mut pages_object: Option<(ObjectId, Object)> = None;
+
+    // Process all objects except "Page" type
+    for (object_id, object) in documents_objects.iter() {
+        // We have to ignore "Page" (as are processed later), "Outlines" and "Outline" objects
+        // All other objects should be collected and inserted into the main Document
+        match object.type_name().unwrap_or("") {
+            "Catalog" => {
+                // Collect a first "Catalog" object and use it for the future "Pages"
+                catalog_object = Some((if let Some((id, _)) = catalog_object { id } else { *object_id }, object.clone()));
+            }
+            "Pages" => {
+                // Collect and update a first "Pages" object and use it for the future "Catalog"
+                // We have also to merge all dictionaries of the old and the new "Pages" object
+                if let Ok(dictionary) = object.as_dict() {
+                    let mut dictionary = dictionary.clone();
+                    if let Some((_, ref object)) = pages_object {
+                        if let Ok(old_dictionary) = object.as_dict() {
+                            dictionary.extend(old_dictionary);
+                        }
+                    }
+
+                    pages_object = Some((if let Some((id, _)) = pages_object { id } else { *object_id }, Object::Dictionary(dictionary)));
+                }
+            }
+            "Page" => {}     // Ignored, processed later and separately
+            "Outlines" => {} // Ignored, not supported yet
+            "Outline" => {}  // Ignored, not supported yet
+            _ => {
+                document.objects.insert(*object_id, object.clone());
+            }
+        }
+    }
+
+    // If no "Pages" found abort
+    if pages_object.is_none() {
+        println!("[merge-pdfs] pages root not found.");
+
+        return Default::default();
+    }
+
+    // Iter over all "Page" and collect with the parent "Pages" created before
+    for (object_id, object) in documents_pages.iter() {
+        if let Ok(dictionary) = object.as_dict() {
+            let mut dictionary = dictionary.clone();
+            dictionary.set("Parent", pages_object.as_ref().unwrap().0);
+
+            document.objects.insert(*object_id, Object::Dictionary(dictionary));
+        }
+    }
+
+    // If no "Catalog" found abort
+    if catalog_object.is_none() {
+        println!("[merge-pdfs] atalog root not found.");
+
+        return Default::default();
+    }
+
+    let catalog_object = catalog_object.unwrap();
+    let pages_object = pages_object.unwrap();
+
+    // Build a new "Pages" with updated fields
+    if let Ok(dictionary) = pages_object.1.as_dict() {
+        let mut dictionary = dictionary.clone();
+
+        // Set new pages count
+        dictionary.set("Count", documents_pages.len() as u32);
+
+        // Set new "Kids" list (collected from documents pages) for "Pages"
+        dictionary.set("Kids", documents_pages.into_iter().map(|(object_id, _)| Object::Reference(object_id)).collect::<Vec<_>>());
+
+        document.objects.insert(pages_object.0, Object::Dictionary(dictionary));
+    }
+
+    // Build a new "Catalog" with updated fields
+    if let Ok(dictionary) = catalog_object.1.as_dict() {
+        let mut dictionary = dictionary.clone();
+        dictionary.set("Pages", pages_object.0);
+        dictionary.remove(b"Outlines"); // Outlines not supported in merged PDFs
+
+        document.objects.insert(catalog_object.0, Object::Dictionary(dictionary));
+    }
+
+    document.trailer.set("Root", catalog_object.0);
+
+    // Update the max internal ID as wasn't updated before due to direct objects insertion
+    document.max_id = document.objects.len() as u32;
+
+    // Reorder all new Document objects
+    document.renumber_objects();
+
+    //Set any Bookmarks to the First child if they are not set to a page
+    document.adjust_zero_pages();
+
+    //Set all bookmarks to the PDF Object tree then set the Outlines to the Bookmark content map.
+    if let Some(n) = document.build_outline() {
+        if let Ok(x) = document.get_object_mut(catalog_object.0) {
+            if let Object::Dictionary(ref mut dict) = x {
+                dict.set("Outlines", Object::Reference(n));
+            }
+        }
+    }
+
+    document.compress();
+
+    // Save the merged PDF
+    let mut buffer = Vec::new();
+    document.save_to(&mut buffer).unwrap();
+    buffer
+}
+
 #[cfg(test)]
 mod tests {
     use crate::db::Database;
@@ -478,7 +627,7 @@ mod tests {
     #[tokio::test(threaded_scheduler)]
     async fn test_interviews() {
         let db = Database::new();
-        refresh_interviews(&db).await;
+        //refresh_interviews(&db).await;
         compile_packets(&db).await;
     }
 }
