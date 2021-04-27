3bdb0f77d880d76e64b05504e880349f67177fd3
Jess Frazelle <jess@oxide.computer>
2021-04-27 13:09:30 -0700

    add formatted address
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- cio/src/templates.rs
+++ cio/src/templates.rs
@@ -367,6 +367,7 @@ resource "okta_user" "{{terraformize this.username}}" {
   primary_phone             = "{{this.recovery_phone}}"{{/if}}{{#if this.recovery_email}}
   second_email              = "{{this.recovery_email}}"{{/if}}
 
+  postal_address     = "{{this.home_address_formatted}}"
   street_address     = "{{this.home_address_street_1}}{{#if this.home_address_street_2}} {{this.home_address_street_2}}{{/if}}"
   city               = "{{this.home_address_city}}"
   state              = "{{this.home_address_state}}"
