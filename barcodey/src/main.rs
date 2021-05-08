9fba07b8bd4b0a08a4e042e110003ce0e48bb694
Jess Frazelle <jess@oxide.computer>
2021-05-08 15:22:33 -0700

    listen to events
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- barcodey/src/main.rs
+++ barcodey/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use hidapi::HidApi;
 use sentry::IntoDsn;
 
 #[tokio::main]
@@ -14,5 +15,46 @@ async fn main() -> Result<(), String> {
         ..Default::default()
     });
 
-    Ok(())
+    let api = HidApi::new().expect("Failed to create API instance");
+    let vendor_id: u16 = u16::MIN;
+    let product_id: u16 = u16::MIN;
+
+    // Iterate over our devices.
+    // Try and find the barcode scanner.
+    for device in api.device_list() {
+        println!(
+            "VID: {:04x}, PID: {:04x}, Serial: {}, Product name: {}",
+            device.vendor_id(),
+            device.product_id(),
+            match device.serial_number() {
+                Some(s) => s,
+                _ => "<COULD NOT FETCH>",
+            },
+            match device.product_string() {
+                Some(s) => s,
+                _ => "<COULD NOT FETCH>",
+            }
+        );
+    }
+
+    if vendor_id == u16::MIN && product_id == u16::MIN {
+        return Err("could not find barcode scanner in HID devices".to_string());
+    }
+
+    // Open the scanner device and listen for events to read.
+    let scanner = api.open(vendor_id, product_id).expect("Failed to open device");
+    println!("Listening for events from (vendor ID: {}) (product ID: {}) in a loop...", vendor_id, product_id);
+
+    loop {
+        let mut buf = [0u8; 256];
+        let res = scanner.read(&mut buf[..]).unwrap();
+
+        let mut data_string = String::new();
+
+        for u in &buf[..res] {
+            data_string.push_str(&(u.to_string() + "\t"));
+        }
+
+        println!("{}", data_string);
+    }
 }
