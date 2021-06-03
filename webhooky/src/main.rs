4c87b1879ef9fecc91c282c566653dd7000f2bd3
Jess Frazelle <jess@oxide.computer>
2021-06-03 06:00:27 -0700

    logging for shipping webhook
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- webhooky/src/main.rs
+++ webhooky/src/main.rs
@@ -1045,6 +1045,8 @@ async fn listen_shippo_tracking_update_webhooks(rqctx: Arc<RequestContext<Contex
         Default::default()
     });
 
+    sentry::capture_message(&format!("shippo: {:?}", body), sentry::Level::Info);
+
     let ts = body.data;
     if ts.address_from.unwrap_or_default().country.is_empty() && ts.tracking_history.is_empty() {
         // We can reaturn early.
