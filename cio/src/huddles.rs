780be916c032bab43bdd30763bf224893354c891
Jess Frazelle <jess@oxide.computer>
2021-07-20 13:05:20 -0700

    fix fmt
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- cio/src/huddles.rs
+++ cio/src/huddles.rs
@@ -94,8 +94,8 @@ pub async fn sync_changes_to_google_events(db: &Database, company: &Company) {
                 let description = format!(
                     "This is the event for {} huddles.
 
-You can submit topics at: https://{}-huddle-form.corp.{} The \
-                     Airtable workspace lives at: https://{}-huddle.corp.{}
+You can submit topics at: https://{}-huddle-form.corp.{} \
+                     The Airtable workspace lives at: https://{}-huddle.corp.{}
 
 {}",
                     slug.replace('-', " "),
