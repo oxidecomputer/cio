1c06024ffb83ef188a9f7d1b75b88b6b45670600
Jess Frazelle <jess@oxide.computer>
2021-08-16 19:45:19 -0700

    make 10 gigs
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- webhooky/src/main.rs
+++ webhooky/src/main.rs
@@ -86,7 +86,7 @@ async fn main() -> Result<(), String> {
      */
     let config_dropshot = ConfigDropshot {
         bind_address: service_address.parse().unwrap(),
-        request_body_max_bytes: 100000000,
+        request_body_max_bytes: 10737418240, // 10 Gigiabytes.
     };
 
     /*
