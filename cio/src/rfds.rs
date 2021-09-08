7b76cc89508298973f939b1a43a4d7f40d0a9823
Jess Frazelle <jess@oxide.computer>
2021-09-08 11:22:22 -0700

    start of function to cleanup stale pdfs
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- cio/src/rfds.rs
+++ cio/src/rfds.rs
@@ -956,6 +956,110 @@ pub async fn refresh_db_rfds(db: &Database, company: &Company) -> Result<()> {
     Ok(())
 }
 
+pub async fn cleanup_rfd_pdfs(db: &Database, company: &Company) -> Result<()> {
+    // Get all the rfds from the database.
+    let rfds = RFDs::get_from_db(db, company.id);
+    let github = company.authenticate_github();
+
+    // Get all the PDF files.
+    let files = github
+        .repos()
+        .get_content_vec_entries(
+            &company.github_org,
+            "rfd",
+            "/pdfs/",
+            "", // leaving the branch blank gives us the default branch
+        )
+        .await?;
+
+    let mut github_pdf_files: BTreeMap<String, String> = Default::default();
+    for file in files {
+        // We will store these in github_pdf_files as <{name}, {sha}>. So we can more easily delete
+        // them.
+        github_pdf_files.insert(file.name.to_string(), file.sha.to_string());
+    }
+
+    let drive_client = company.authenticate_google_drive(db).await.unwrap();
+
+    // Figure out where our directory is.
+    // It should be in the shared drive : "Automated Documents"/"rfds"
+    let shared_drive = drive_client.drives().get_by_name("Automated Documents").await?;
+    let drive_id = shared_drive.id.to_string();
+
+    // Get the directory by the name.
+    let parent_id = drive_client.files().create_folder(&drive_id, "", "rfds").await?;
+
+    // Iterate over the RFD and cleanup any PDFs with the wrong name.
+    for rfd in rfds {
+        let pdf_file_name = rfd.get_pdf_filename();
+
+        // First let's do Google Drive.
+        // Search for files with that rfd number string.
+        let drive_files = drive_client
+            .files()
+            .list_all(
+                "drive",                                                                           // corpa
+                &drive_id,                                                                         // drive id
+                true,  // include items from all drives
+                "",    // include permissions for view
+                false, // include team drive items
+                "",    // order by
+                &format!("name contains '{}' and '{}' in parents", &rfd.number_string, parent_id), // query
+                "",    // spaces
+                true,  // supports all drives
+                false, // supports team drives
+                "",    // team drive id
+            )
+            .await?;
+        // Iterate over the files and if the name does not equal our name, then nuke it.
+        for df in drive_files {
+            println!("drive file: {:#?}", df);
+            // Delete the file from our drive.
+            //drive_client.files().delete(&df.id, true, true).await?;
+        }
+
+        // Now let's do GitHub.
+        // Iterate over our github_pdf_files and delete any that do not match.
+        for (gf_name, sha) in github_pdf_files.clone() {
+            if gf_name == pdf_file_name {
+                println!("[{}] Keeping GitHub PDF: {}", rfd.number_string, gf_name);
+                // Remove it from our btree map.
+                github_pdf_files.remove(&gf_name);
+                continue;
+            }
+
+            if gf_name.contains(&rfd.number_string) {
+                // Remove it from GitHub.
+                println!("[{}] Deleting GitHub PDF: {}", rfd.number_string, gf_name);
+                github
+                    .repos()
+                    .delete_file(
+                        &company.github_org,
+                        "rfd",
+                        &format!("pdfs/{}", gf_name),
+                        &octorust::types::ReposDeleteFileRequest {
+                            message: format!(
+                                "Deleting file content {} programatically\n\nThis is done from \
+                                 the cio repo cio::cleanup_rfd_pdfs function.",
+                                gf_name
+                            ),
+                            sha: sha.to_string(),
+                            committer: None,
+                            author: None,
+                            branch: "".to_string(),
+                        },
+                    )
+                    .await?;
+
+                // Remove it from our btree map.
+                github_pdf_files.remove(&gf_name);
+            }
+        }
+    }
+
+    Ok(())
+}
+
 /// Create a changelog email for the RFDs.
 pub async fn send_rfd_changelog(company: &Company) {
     // Initialize our database.
@@ -1004,10 +1108,23 @@ mod tests {
         companies::Company,
         db::Database,
         rfds::{
-            clean_rfd_html_links, refresh_db_rfds, send_rfd_changelog, update_discussion_link, update_state, NewRFD,
-            RFDs,
+            clean_rfd_html_links, cleanup_rfd_pdfs, refresh_db_rfds, send_rfd_changelog, update_discussion_link,
+            update_state, NewRFD, RFDs,
         },
     };
+
+    #[ignore]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_rfds_cleanup_pdfs() {
+        // Initialize our database.
+        let db = Database::new();
+
+        // Get the company id for Oxide.
+        // TODO: split this out per company.
+        let oxide = Company::get_from_db(&db, "Oxide".to_string()).unwrap();
+
+        cleanup_rfd_pdfs(&db, &oxide).await.unwrap();
+    }
 
     #[ignore]
     #[tokio::test(flavor = "multi_thread")]
