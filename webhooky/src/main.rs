0edcca542654cd494f172a2a33d27c4ba16c2219
Jess Frazelle <jess@oxide.computer>
2021-09-09 11:33:22 -0700

    refactor more callbacks
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- webhooky/src/main.rs
+++ webhooky/src/main.rs
@@ -15,15 +15,8 @@ use std::{collections::HashMap, env, fs::File, sync::Arc};
 use anyhow::Result;
 use chrono::offset::Utc;
 use cio_api::{
-    analytics::NewPageView,
-    api_tokens::{APIToken, NewAPIToken},
-    applicants::Applicant,
-    companies::Company,
-    db::Database,
-    mailing_list::MailingListSubscriber,
-    rack_line::RackLineSubscriber,
-    schema::{api_tokens, applicants},
-    swag_store::Order,
+    analytics::NewPageView, api_tokens::NewAPIToken, applicants::Applicant, companies::Company, db::Database,
+    mailing_list::MailingListSubscriber, rack_line::RackLineSubscriber, schema::applicants, swag_store::Order,
 };
 use diesel::prelude::*;
 use docusign::DocuSign;
@@ -1223,59 +1216,11 @@ async fn listen_auth_ramp_callback(
     query_args: Query<AuthCallback>,
 ) -> Result<HttpResponseAccepted<String>, HttpError> {
     sentry::start_session();
-    let api_context = rqctx.context();
-    let event = query_args.into_inner();
 
-    // Initialize the Ramp client.
-    let mut g = Ramp::new_from_env("", "");
-
-    // Let's get the token from the code.
-    let t = g.get_access_token(&event.code, &event.state).await.unwrap();
-
-    let ru = g
-        .users()
-        .get_all(
-            "", // department id
-            "", // location id
-        )
-        .await
-        .unwrap();
-
-    // Let's get the domain from the email.
-    let mut domain = "".to_string();
-    if !ru.is_empty() {
-        let split = ru.get(0).unwrap().email.split('@');
-        let vec: Vec<&str> = split.collect();
-        if vec.len() > 1 {
-            domain = vec.get(1).unwrap().to_string();
-        }
+    if let Err(e) = crate::handlers_auth::handle_auth_ramp_callback(rqctx, query_args).await {
+        // Send the error to sentry.
+        return Err(handle_anyhow_err_as_http_err(e));
     }
-
-    let company = Company::get_from_domain(&api_context.db, &domain).unwrap();
-
-    // Save the token to the database.
-    let mut token = NewAPIToken {
-        product: "ramp".to_string(),
-        token_type: t.token_type.to_string(),
-        access_token: t.access_token.to_string(),
-        expires_in: t.expires_in as i32,
-        refresh_token: t.refresh_token.to_string(),
-        refresh_token_expires_in: t.refresh_token_expires_in as i32,
-        company_id: "".to_string(),
-        item_id: "".to_string(),
-        user_email: "".to_string(),
-        last_updated_at: Utc::now(),
-        expires_date: None,
-        refresh_token_expires_date: None,
-        endpoint: "".to_string(),
-        auth_company_id: company.id,
-        company: Default::default(),
-        // THIS SHOULD ALWAYS BE OXIDE SO THAT IT SAVES TO OUR AIRTABLE.
-        cio_company_id: 1,
-    };
-    token.expand();
-    // Update it in the database.
-    token.upsert(&api_context.db).await.unwrap();
 
     sentry::end_session();
     Ok(HttpResponseAccepted("ok".to_string()))
@@ -1310,117 +1255,10 @@ async fn listen_auth_slack_callback(
     query_args: Query<AuthCallback>,
 ) -> Result<HttpResponseAccepted<String>, HttpError> {
     sentry::start_session();
-    let api_context = rqctx.context();
-    let event = query_args.into_inner();
 
-    // Initialize the Slack client.
-    let mut s = Slack::new_from_env("", "", "");
-
-    // Let's get the token from the code.
-    let t = s.get_access_token(&event.code).await.unwrap();
-
-    // Get the current user.
-    let current_user = s.current_user().await.unwrap();
-
-    // Let's get the domain from the email.
-    let split = current_user.email.split('@');
-    let vec: Vec<&str> = split.collect();
-    let mut domain = "".to_string();
-    if vec.len() > 1 {
-        domain = vec.get(1).unwrap().to_string();
-    }
-
-    let company = Company::get_from_domain(&api_context.db, &domain).unwrap();
-
-    let mut webhook = "".to_string();
-    if let Some(wh) = t.incoming_webhook {
-        webhook = wh.url;
-    }
-
-    // Save the bot token to the database.
-    let mut token = NewAPIToken {
-        product: "slack".to_string(),
-        token_type: t.token_type.to_string(),
-        access_token: t.access_token.to_string(),
-        expires_in: 0,
-        refresh_token: "".to_string(),
-        refresh_token_expires_in: 0,
-        company_id: t.team.id.to_string(),
-        item_id: t.team.name.to_string(),
-        user_email: current_user.email.to_string(),
-        last_updated_at: Utc::now(),
-        expires_date: None,
-        refresh_token_expires_date: None,
-        endpoint: webhook.to_string(),
-        auth_company_id: company.id,
-        company: Default::default(),
-        // THIS SHOULD ALWAYS BE OXIDE SO THAT IT SAVES TO OUR AIRTABLE.
-        cio_company_id: 1,
-    };
-    token.expand();
-
-    // Update it in the database.
-    let mut new_token = if let Ok(existing) = api_tokens::dsl::api_tokens
-        .filter(
-            api_tokens::dsl::cio_company_id
-                .eq(1)
-                .and(api_tokens::dsl::product.eq("slack".to_string()))
-                .and(api_tokens::dsl::auth_company_id.eq(company.id))
-                .and(api_tokens::dsl::token_type.eq(token.token_type.to_string())),
-        )
-        .first::<APIToken>(&api_context.db.conn())
-    {
-        diesel::update(&existing)
-            .set(token)
-            .get_result::<APIToken>(&api_context.db.conn())
-            .unwrap_or_else(|e| panic!("unable to update record {}: {}", existing.id, e))
-    } else {
-        token.create_in_db(&api_context.db).unwrap()
-    };
-    new_token.upsert_in_airtable(&api_context.db).await.unwrap();
-
-    // Save the user token to the database.
-    if let Some(authed_user) = t.authed_user {
-        let mut user_token = NewAPIToken {
-            product: "slack".to_string(),
-            token_type: authed_user.token_type.to_string(),
-            access_token: authed_user.access_token.to_string(),
-            expires_in: 0,
-            refresh_token: "".to_string(),
-            refresh_token_expires_in: 0,
-            company_id: t.team.id.to_string(),
-            item_id: t.team.name.to_string(),
-            user_email: current_user.email.to_string(),
-            last_updated_at: Utc::now(),
-            expires_date: None,
-            refresh_token_expires_date: None,
-            endpoint: webhook.to_string(),
-            auth_company_id: company.id,
-            company: Default::default(),
-            // THIS SHOULD ALWAYS BE OXIDE SO THAT IT SAVES TO OUR AIRTABLE.
-            cio_company_id: 1,
-        };
-        user_token.expand();
-
-        // Update it in the database.
-        let mut new_user_token = if let Ok(existing) = api_tokens::dsl::api_tokens
-            .filter(
-                api_tokens::dsl::cio_company_id
-                    .eq(1)
-                    .and(api_tokens::dsl::product.eq("slack".to_string()))
-                    .and(api_tokens::dsl::auth_company_id.eq(company.id))
-                    .and(api_tokens::dsl::token_type.eq(user_token.token_type.to_string())),
-            )
-            .first::<APIToken>(&api_context.db.conn())
-        {
-            diesel::update(&existing)
-                .set(user_token)
-                .get_result::<APIToken>(&api_context.db.conn())
-                .unwrap_or_else(|e| panic!("unable to update record {}: {}", existing.id, e))
-        } else {
-            user_token.create_in_db(&api_context.db).unwrap()
-        };
-        new_user_token.upsert_in_airtable(&api_context.db).await.unwrap();
+    if let Err(e) = crate::handlers_auth::handle_auth_slack_callback(rqctx, query_args).await {
+        // Send the error to sentry.
+        return Err(handle_anyhow_err_as_http_err(e));
     }
 
     sentry::end_session();
