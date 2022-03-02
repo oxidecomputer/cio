2b062f7b09714e5e77232e18045c6c1b7220d762
Jess Frazelle <github@jessfraz.com>
2022-03-02 11:46:03 -0800

    turn off db tracing again cc #72
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- cio/src/db.rs
+++ cio/src/db.rs
@@ -4,7 +4,6 @@ use anyhow::Result;
 use async_bb8_diesel::ConnectionManager;
 use async_trait::async_trait;
 use diesel::PgConnection;
-use diesel_sentry::SentryConnection;
 
 #[derive(Debug, Clone)]
 pub struct Database {
@@ -12,7 +11,7 @@ pub struct Database {
 }
 
 #[derive(Clone)]
-struct DB(bb8::Pool<ConnectionManager<SentryConnection<diesel::PgConnection>>>);
+struct DB(bb8::Pool<ConnectionManager<diesel::PgConnection>>);
 
 // This is a workaround so we can implement Debug for PgConnection.
 impl fmt::Debug for DB {
@@ -27,7 +26,7 @@ impl Database {
     pub async fn new() -> Self {
         let database_url = env::var("CIO_DATABASE_URL").expect("CIO_DATABASE_URL must be set");
 
-        let manager = ConnectionManager::<SentryConnection<PgConnection>>::new(&database_url);
+        let manager = ConnectionManager::<PgConnection>::new(&database_url);
         let pool = bb8::Pool::builder().max_size(25).build(manager).await.unwrap();
 
         Database { pool: DB(pool) }
@@ -35,7 +34,7 @@ impl Database {
 
     /// Returns a connection from the pool.
     #[tracing::instrument]
-    pub fn pool(&self) -> bb8::Pool<ConnectionManager<SentryConnection<diesel::PgConnection>>> {
+    pub fn pool(&self) -> bb8::Pool<ConnectionManager<diesel::PgConnection>> {
         self.pool.0.clone()
     }
 }
