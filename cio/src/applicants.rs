b0e492a58f4a1ba5240cb2f0899b16b90d1a4c0d
Jess Frazelle <github@jessfraz.com>
2022-03-14 15:35:19 -0700

    bump toolchain
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- cio/src/applicants.rs
+++ cio/src/applicants.rs
@@ -1794,7 +1794,7 @@ impl Applicant {
         // Since we are an Applicant.
         if !self.airtable_record_id.is_empty() {
             // We could URL-encode the whole thing, but we don't need to, just the + is fine.
-            self.scoring_form_url = format!("https://apply.oxide.computer/review/{}", self.email.replace("+", "%2B"));
+            self.scoring_form_url = format!("https://apply.oxide.computer/review/{}", self.email.replace('+', "%2B"));
         }
 
         // Check if we have sent them an email that we received their application.
