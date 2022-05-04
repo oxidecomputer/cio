1e297fa36a07b21bebf4de123c55ffb8d5df08cf
Augustus Mayo <gusmayo@gmail.com>
2022-05-04 10:26:18 -0500

    Initial work on Zoho client with hacky generated types

---
+++ zoho/src/lib.rs
@@ -0,0 +1,10 @@
+/*!
+ * A rust library for interacting with the Zoho API
+ *
+ * For more information, the Zoho API is documented at [zoho.com/bigin/developer/docs/apis](https://www.zoho.com/bigin/developer/docs/apis/)
+ *
+ */
+pub mod client;
+pub mod modules;
+
+pub use client::*;
