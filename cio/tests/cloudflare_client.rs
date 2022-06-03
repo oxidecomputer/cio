7f5fd57d675ec6ab7961a11f21cc8b6b2df28e84
Augustus Mayo <gusmayo@gmail.com>
2022-06-03 11:38:06 -0500

    Add record lookup to test

--- cio/tests/cloudflare_client.rs
+++ cio/tests/cloudflare_client.rs
@@ -12,4 +12,19 @@ async fn test_zone_identifier_lookup_uses_cache() {
 
     assert_eq!(zone_req1.id, zone_req2.id);
     assert_eq!(zone_req1.expires_at, zone_req2.expires_at);
+
+    let dns_records = cf
+        .request(&cloudflare::endpoints::dns::ListDnsRecords {
+            zone_identifier: &zone_req1.id,
+            params: cloudflare::endpoints::dns::ListDnsRecordsParams {
+                // From: https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+                per_page: Some(123),
+                ..Default::default()
+            },
+        })
+        .await
+        .unwrap()
+        .result;
+
+    assert_eq!(123, dns_records.len());
 }
