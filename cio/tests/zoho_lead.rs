6f89e55e108b8f1a77e8c1e657965a6b9c4172d9
Augustus Mayo <gusmayo@gmail.com>
2022-06-07 10:57:57 -0500

    Mock out test

--- cio/tests/zoho_lead.rs
+++ cio/tests/zoho_lead.rs
@@ -1,5 +1,9 @@
-use cio_api::rack_line::RackLineSubscriber;
+use cio_api::{
+    rack_line::RackLineSubscriber,
+    zoho::push_new_rack_line_subscribers_to_zoho
+};
 
+#[ignore]
 #[tokio::test]
 async fn test_pushes_lead() {
     let db = cio_api::db::Database::new().await;
@@ -9,8 +13,9 @@ async fn test_pushes_lead() {
 
     let lead_id = std::env::var("LEAD_ID").unwrap().parse::<i32>().unwrap();
     let subscriber = RackLineSubscriber::get_by_id(&db, lead_id).await.unwrap();
+    let mut subscribers = vec![subscriber];
 
-    println!("{:#?}", subscriber);
+    let push_result = push_new_rack_line_subscribers_to_zoho(&mut subscribers).await;
 
-    panic!("exit");
+    assert!(push_result.is_ok());
 }
