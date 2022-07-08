edb5dc6faa8e171165d1ec662b3189b0cfd1c2a2
Augustus Mayo <gusmayo@gmail.com>
2022-07-08 08:59:52 -0500

    Minor comments

--- cio/src/companies.rs
+++ cio/src/companies.rs
@@ -1059,10 +1059,13 @@ impl Company {
         ))
     }
 
+    // TODO: Extract out the hardcoded repo name so that it can be configurable
     pub fn rfd_repo_name(&self) -> &str {
         "rfd"
     }
 
+    // Creates a minimal type for callers that need information about the RFD repo, but
+    // do not want to parse data from the full API response
     pub async fn rfd_repo(&self) -> Result<RFDRepo> {
         self.authenticate_github()?
             .repos()
