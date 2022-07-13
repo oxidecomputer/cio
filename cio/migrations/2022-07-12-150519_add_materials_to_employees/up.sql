cc421d935c7aac24ff0fb60b3fcbc9593fff5c4c
Augustus Mayo <gusmayo@gmail.com>
2022-07-13 11:41:38 -0500

    Copy material urls based on implicit logic during initial migration

--- cio/migrations/2022-07-12-150519_add_materials_to_employees/up.sql
+++ cio/migrations/2022-07-12-150519_add_materials_to_employees/up.sql
@@ -1,1 +1,15 @@
-ALTER TABLE users ADD COLUMN materials VARCHAR NOT NULL '';
+ALTER TABLE users ADD COLUMN materials VARCHAR NOT NULL DEFAULT '';
+
+UPDATE
+  users
+SET
+  materials = applicant.materials
+FROM (
+  SELECT
+    email,
+    materials
+  FROM
+    applicants
+) AS applicant
+WHERE
+  users.recovery_email = applicant.email;
