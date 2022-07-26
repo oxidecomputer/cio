b39f4b47de3d02571bd0a5991dfd95a01c202e4f
Augustus Mayo <gusmayo@gmail.com>
2022-07-26 11:34:47 -0500

    Add docs for partial macro

--- partial-struct/src/lib.rs
+++ partial-struct/src/lib.rs
@@ -15,12 +15,19 @@ use syn::{
     AttributeArgs, DeriveInput, Ident, LitStr, Result, Token,
 };
 
+/// Optional commands that can be define on a per field level. Commands are paired with the
+/// specific struct name that it applies to.
+///
+/// Commands:
+///   skip - Omits the field from the newly generated struct
 #[derive(Debug)]
 struct FieldCommands {
     name: Ident,
     skip: bool,
 }
 
+/// Currently field command syntax is a comma separated list of values. This will likely be
+/// replaced with a more flexible syntax in the future
 impl Parse for FieldCommands {
     fn parse(input: ParseStream) -> Result<Self> {
         let name = input.parse()?;
@@ -28,6 +35,8 @@ impl Parse for FieldCommands {
         let content;
         syn::parenthesized!(content in input);
 
+        // Invalid or unknown commands are currently ignored. They likely will be upgraded to
+        // to errors in the future
         let commands: Punctuated<Ident, Token![,]> = content.parse_terminated(Ident::parse)?;
 
         let skip = commands.iter().any(|c| c == "skip");
@@ -36,32 +45,41 @@ impl Parse for FieldCommands {
     }
 }
 
+/// A holder of derive identifiers to add or remove from generated structs
 #[derive(Debug, Clone)]
-struct TraitOptions {
+struct DeriveOptions {
     traits: Vec<Ident>,
 }
 
-impl Parse for TraitOptions {
+impl Parse for DeriveOptions {
     fn parse(input: ParseStream) -> Result<Self> {
         let mut traits = vec![];
 
         while !input.is_empty() {
             let t: Ident = input.parse()?;
+
+            // We do not try to validate the derive, if an invalid derive is supplied then it
+            // will fail during compilation later on
             traits.push(t);
 
             // Parse an optional comma following each trait
             let _: Result<Token![,]> = input.parse();
         }
 
-        Ok(TraitOptions { traits })
+        Ok(DeriveOptions { traits })
     }
 }
 
+/// A parsed out struct that has been requested to be created
 #[derive(Debug)]
 struct NewStruct {
     name: Ident,
-    with: Option<TraitOptions>,
-    without: Option<TraitOptions>,
+
+    // Additional derives that should be added to the struct
+    with: Option<DeriveOptions>,
+
+    // Derives that should be removed from the new struct (if they exist)
+    without: Option<DeriveOptions>,
 }
 
 impl Parse for NewStruct {
@@ -84,12 +102,12 @@ impl Parse for NewStruct {
             if option == "with" {
                 let to_add: Group = input.parse()?;
                 let tokens = to_add.stream();
-                let traits: TraitOptions = syn::parse2(tokens)?;
+                let traits: DeriveOptions = syn::parse2(tokens)?;
                 new_struct.with = Some(traits);
             } else if option == "without" {
                 let to_add: Group = input.parse()?;
                 let tokens = to_add.stream();
-                let traits: TraitOptions = syn::parse2(tokens)?;
+                let traits: DeriveOptions = syn::parse2(tokens)?;
                 new_struct.without = Some(traits);
             } else {
                 return Err(syn::Error::new(option.span(), "unknown option"));
@@ -125,8 +143,8 @@ impl Parse for Derives {
 
 fn compute_derives(
     mut attr: syn::Attribute,
-    with: Option<TraitOptions>,
-    without: Option<TraitOptions>,
+    with: Option<DeriveOptions>,
+    without: Option<DeriveOptions>,
 ) -> syn::Attribute {
     let derives: Result<Derives> = syn::parse2(attr.tokens);
     let mut derive_list = derives.map(|d| d.derives).unwrap_or_else(|_| vec![]);
@@ -148,13 +166,42 @@ fn compute_derives(
     attr
 }
 
+/// A macro (for structs) that generates a new struct containing a subset of the fields of the
+/// tagged struct. New structs can have additional derive values added or removed. Any subsequent
+/// (non-partial) derives or macros will be applied to the new structs as well.
+///
+/// Examples:
+///
+/// ```
+/// // Using all of the macro features
+///
+/// #[partial(NewStruct, with(Debug), without(Default))]
+/// #[derive(Default)]
+/// struct OldStruct {
+///    a: u32
+///    #[partial(NewStruct(skip))]
+///    b: u32
+/// }
+///
+/// // will generate
+///
+/// #[derive(Debug)]
+/// struct NewStruct {
+///    a: u32
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn partial(attr: TokenStream, input: TokenStream) -> TokenStream {
-    let mut input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    let original_name = input.ident.clone();
+    // We will need this name later when generating [From] impls
+    let original_name = input.ident;
 
+    // Parse the attribute that triggered this macro to execute
     let first_new_struct = parse_macro_input!(attr as NewStruct);
+
+    // Look through the remaining attributes on this struct and find any other instances of the
+    // [partial] macro. For each of those, parse them into their own [NewStruct]
     let additional_structs: Result<Vec<NewStruct>> = input
         .attrs
         .iter()
@@ -169,6 +216,9 @@ pub fn partial(attr: TokenStream, input: TokenStream) -> TokenStream {
 
     match additional_structs {
         Ok(mut additional_structs) => {
+
+            // Construct the full list of structs that need to be created. This list needs to
+            // include the original struct (without modification) as well
             let mut new_structs = vec![
                 NewStruct {
                     name: original_name.clone(),
@@ -180,38 +230,52 @@ pub fn partial(attr: TokenStream, input: TokenStream) -> TokenStream {
             new_structs.append(&mut additional_structs);
 
             let result = match input.data {
-                syn::Data::Struct(ref mut s) => {
+
+                // This macro is only defined for structs. Usage on any other data type will
+                // fail with a panic
+                syn::Data::Struct(ref s) => {
                     let visibility = input.vis;
                     let generics = input.generics;
 
+                    // Create the list of all attribute macros that need to be applied to the
+                    // generated structs
                     let attr_without_partials: Vec<&syn::Attribute> = input
                         .attrs
                         .iter()
                         .filter(|attr| !attr.path.is_ident("partial"))
                         .collect();
 
+                    // From the list of attributes, find all of the non-derive attributes
                     let struct_attrs: Vec<&syn::Attribute> = attr_without_partials
                         .into_iter()
                         .filter(|attr| !attr.path.is_ident("derive"))
                         .collect();
 
+                    // Find the derive attribute if one exists
                     let orig_derives: Option<&syn::Attribute> =
                         input.attrs.iter().filter(|attr| attr.path.is_ident("derive")).nth(0);
 
+                    // Keep track of all of the structs to output
                     let mut expanded_structs = vec![];
 
-                    if let syn::Fields::Named(ref mut fields) = s.fields {
+                    if let syn::Fields::Named(ref fields) = s.fields {
+
+                        // Generate each of the requested structs
                         for new_struct in new_structs {
                             let NewStruct { name, with, without } = new_struct;
-                            let visibility = visibility.clone();
-                            let generics = generics.clone();
 
+                            // Generate the list of fields to assign to the new struct
                             let filtered_fields: Vec<syn::Field> = fields
                                 .named
                                 .iter()
+                                // Omit any fields with the `partial` attribute that include the
+                                // `skip` command
                                 .filter(|field| {
                                     !field.attrs.iter().any(|attr| {
                                         if attr.path.is_ident("partial") {
+                                            
+                                            // This ideally would be a helpful error message instead
+                                            // of a panic
                                             let parsed: FieldCommands =
                                                 attr.parse_args().expect("Failed to parse field args");
                                             parsed.name == name && parsed.skip
@@ -220,22 +284,23 @@ pub fn partial(attr: TokenStream, input: TokenStream) -> TokenStream {
                                         }
                                     })
                                 })
-                                .map(|f| {
-                                    let mut f = f.to_owned();
+                                .map(|field| {
+                                    let mut field = field.to_owned();
 
-                                    f.attrs = f
+                                    // Filter out any `partial` attributes assigned to the field
+                                    field.attrs = field
                                         .attrs
                                         .into_iter()
                                         .filter(|attr| !attr.path.is_ident("partial"))
                                         .collect();
 
-                                    f
+                                    field
                                 })
                                 .collect();
 
-                            // If there were no additional derives added to the new struct than we
-                            // can add a From impl to convert from the original struct to the partial
-                            let from_impl = if name != original_name && with.is_none() {
+                            // If this is not the original struct being generated, create a default
+                            // From impl from the original struct to the new struct.
+                            let from_impl = if name != original_name {
                                 let field_names = filtered_fields
                                     .iter()
                                     .map(|field| field.ident.as_ref())
@@ -255,7 +320,13 @@ pub fn partial(attr: TokenStream, input: TokenStream) -> TokenStream {
                             };
 
                             let derives: Option<syn::Attribute> = orig_derives.map(|d| d.to_owned());
+
                             let derive_attr = if let Some(derives) = derives {
+
+                                // Add in and/or remove the additional derives defined by the caller.
+                                // Adding derives may result in further compilation errors in the
+                                // fields in the original struct are not compatible with the new
+                                // derives
                                 let computed = compute_derives(derives, with, without);
                                 quote! { #computed }
                             } else {
@@ -278,6 +349,8 @@ pub fn partial(attr: TokenStream, input: TokenStream) -> TokenStream {
                         #( #expanded_structs )*
                     })
                 }
+
+                // Ideally this would return a descriptive error instead of panicking
                 other => panic!(
                     "Partial can only be defined on structs. Attempted to define on {:#?}",
                     other
